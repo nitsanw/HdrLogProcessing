deb02ff38610f3e1e97a336c801927565cbc3c85
nitsanw <nitsanw@yahoo.com>
2016-05-09 09:51:52 +0200

    Use histogram tags in summary. Add a log union utility.
    Requires HdrHistogram 2.1.9-SNAPSHOT ATM.

--- src/main/java/SummarizeHistogramLogRange.java
+++ src/main/java/SummarizeHistogramLogRange.java
@@ -1,10 +1,11 @@
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
@@ -16,15 +17,69 @@ import org.kohsuke.args4j.Option;
 
 public class SummarizeHistogramLogRange {
 
-    @Option(name = "-s", usage = "relative log start time in seconds, defaults to 0.0", required = false)
+    enum SummaryType {
+        CSV, PERCENTILES, HGRM
+    }
+
+    @Option(name="-ignoreTag", aliases="-it", usage="summary should not be split by tag, (default: false)", required=false)
+    public boolean ignoreTag = false;
+
+    @Option(name="-start", aliases="-s", usage="relative log start time in seconds, (default: 0.0)", required=false)
     public double start = 0.0;
-    @Option(name = "-e", usage = "relative log end time in seconds, defaults to MAX_DOUBLE", required = false)
+
+    @Option(name="-end", aliases="-e", usage="relative log end time in seconds, (default: MAX_DOUBLE)", required=false)
     public double end = Double.MAX_VALUE;
 
-    private File inputPath = new File(".");
-    Set<File> inputFiles = new HashSet<>();
-    @Option(name = "-v", usage = "verbose logging, defaults to false", required = false)
+    @Option(name="-verbose", aliases="-v", usage="verbose logging, (default: false)", required=false)
     public boolean verbose = false;
+
+    @Option(name="-summaryType", aliases="-st", usage="summary type: csv, percentiles, hgrm", required=false)
+    public SummaryType summaryType = SummaryType.PERCENTILES;
+
+    @Option(name="-percentilesOutputTicksPerHalf", aliases="-tph", usage="ticks per half percentile, used for hgrm output, (default: 5)", required=false)
+    public int percentilesOutputTicksPerHalf = 5;
+
+    @Option(name="-outputValueUnitRatio", aliases="-ovr", usage="output value unit ratio, (default: 1.0)", required=false)
+    public double outputValueUnitRatio = 1.0;
+
+    @Option(name="-outputBucketSize", aliases="-obs", usage="csv output bucket size, (default: 100)", required=false)
+    public long outputBucketSize = 100;
+
+    @Option(name="-inputPath", aliases="-ip", usage="set path to use for input files, defaults to current folder", required=false)
+    public void setInputPath(String inputFolderName) {
+        inputPath = new File(inputFolderName);
+        if (!inputPath.exists())
+            throw new IllegalArgumentException("inputPath:" + inputFolderName + " must exist!");
+        if (!inputPath.isDirectory())
+            throw new IllegalArgumentException("inputPath:" + inputFolderName + " must be a directory!");
+    }
+
+    @Option(name="-inputFile", aliases="-if", usage="add an input hdr log from input path, also takes regexp", required=false)
+    public void addInputFile(String inputFile) {
+        final Predicate<String> predicate = Pattern.compile(inputFile).asPredicate();
+        inputFiles.addAll(
+          Arrays.asList(
+            inputPath.listFiles(pathname -> {return predicate.test(pathname.getName());})
+          )
+        );
+    }
+
+    @Option(name="-inputFilePath", aliases="-ifp", usage="add an input file by path relative to working dir or absolute", required=false)
+    public void addInputFileAbs(String inputFileName) {
+        File in = new File(inputFileName);
+        if (!in.exists())
+            throw new IllegalArgumentException("file:" + inputFileName + " must exist!");
+        inputFiles.add(in);
+    }
+
+    @Option(name="-outputFile", aliases="-of", usage="set an output file destination, default goes to sysout", required=false)
+    public void setOutputFile(String outputFileName) {
+        outputFile = new File(outputFileName);
+    }
+
+
+    private File inputPath = new File(".");
+    private Set<File> inputFiles = new HashSet<>();
     private File outputFile;
 
     public static void main(String[] args) throws Exception {
@@ -39,8 +94,7 @@ public class SummarizeHistogramLogRange {
         }
     }
 
-    public void execute() throws FileNotFoundException {
-
+    private void execute() throws FileNotFoundException {
         if (verbose) {
             if (end != Double.MAX_VALUE)
                 System.out.printf("start:%.2f end:%.2f path:%s\n", start, end, inputPath.getAbsolutePath());
@@ -50,74 +104,109 @@ public class SummarizeHistogramLogRange {
         if (inputFiles.isEmpty())
             return;
         PrintStream report = System.out;
-        if(outputFile != null) {
+        if (outputFile != null) {
             report = new PrintStream(new FileOutputStream(outputFile));
         }
         summarizeAndPrint(report);
     }
 
     private void summarizeAndPrint(PrintStream out) throws FileNotFoundException {
-        Histogram sum = new Histogram(3);
+        Map<String, Histogram> sumByTag= new HashMap<>();
+
         long period = 0;
         for (File inputFile : inputFiles) {
             if (verbose)
-                System.out.println("Summarizing file:" + inputFile.getName());
+                System.out.println("Summarizing file: " + inputFile.getName());
             HistogramLogReader reader = new HistogramLogReader(inputFile);
             Histogram interval;
+            int i = 0;
             while ((interval = (Histogram) reader.nextIntervalHistogram(start, end)) != null) {
+                String ntag = ignoreTag ? null : interval.getTag();
+                Histogram sum = sumByTag.computeIfAbsent(ntag, k -> {Histogram h = new Histogram(3); h.setTag(k); return h;});
                 sum.add(interval);
+                if (verbose) {
+                    String tag = (sum.getTag() == null) ? "" : "("+sum.getTag()+") ";
+                    System.out.printf("%s%d: [count=%d,min=%d,max=%d,avg=%.2f,50=%d,99=%d,999=%d]\n",
+                            tag, i++,
+                            interval.getTotalCount(),
+                            (long) (interval.getMinValue() / outputValueUnitRatio),
+                            (long) (interval.getMaxValue() / outputValueUnitRatio),
+                            interval.getMean() / outputValueUnitRatio,
+                            (long) (interval.getValueAtPercentile(50) / outputValueUnitRatio),
+                            (long) (interval.getValueAtPercentile(99) / outputValueUnitRatio),
+                            (long) (interval.getValueAtPercentile(99.9) / outputValueUnitRatio));
+                }
             }
-            long sumPeriod = (sum.getEndTimeStamp() - sum.getStartTimeStamp());
-            sum.setEndTimeStamp(0);
-            sum.setStartTimeStamp(Long.MAX_VALUE);
-            period += sumPeriod;
+            long maxPeriod = 0;
+            for (Histogram sum : sumByTag.values()) {
+                long sumPeriod = (sum.getEndTimeStamp() - sum.getStartTimeStamp());
+                if (verbose) {
+                    String tag = (sum.getTag() == null) ? "" : "::"+sum.getTag();
+                    System.out.printf("%s%s %d secs [count=%d,min=%d,max=%d,avg=%.2f,50=%d,99=%d,999=%d]\n",
+                            inputFile.getName(), tag,
+                            sumPeriod / 1000, sum.getTotalCount(),
+                            (long) (sum.getMinValue() / outputValueUnitRatio),
+                            (long) (sum.getMaxValue() / outputValueUnitRatio), sum.getMean() / outputValueUnitRatio,
+                            (long) (sum.getValueAtPercentile(50) / outputValueUnitRatio),
+                            (long) (sum.getValueAtPercentile(99) / outputValueUnitRatio),
+                            (long) (sum.getValueAtPercentile(99.9) / outputValueUnitRatio));
+                }
+                sum.setEndTimeStamp(0);
+                sum.setStartTimeStamp(Long.MAX_VALUE);
+                maxPeriod = Math.max(maxPeriod, sumPeriod);
+            }
+            period += maxPeriod;
         }
-
-        double avgThpt = (sum.getTotalCount() * 1000.0) / period;
-        out.printf("TotalCount=%d\n", sum.getTotalCount());
-        out.printf("Period(ms)=%d\n", period);
-        out.printf("Throughput(ops/sec)=%.2f\n", avgThpt);
-        out.printf("Min=%d\n", sum.getMinValue());
-        out.printf("Mean=%.2f\n", sum.getMean());
-        out.printf("50.000ptile=%d\n", sum.getValueAtPercentile(50));
-        out.printf("90.000ptile=%d\n", sum.getValueAtPercentile(90));
-        out.printf("99.000ptile=%d\n", sum.getValueAtPercentile(99));
-        out.printf("99.900ptile=%d\n", sum.getValueAtPercentile(99.9));
-        out.printf("99.990ptile=%d\n", sum.getValueAtPercentile(99.99));
-        out.printf("99.999ptile=%d\n", sum.getValueAtPercentile(99.999));
-        out.printf("Max=%d\n", sum.getMaxValue());
-    }
-
-    @Option(name = "-inputPath", aliases = {"-ip" }, usage = "set path to use for input files, defaults to current folder", required = false)
-    public void setInputPath(String inputFolderName) {
-        inputPath = new File(inputFolderName);
-        if (!inputPath.exists())
-            throw new IllegalArgumentException("inputPath:"+inputFolderName+" must exist!");
-        if (!inputPath.isDirectory())
-            throw new IllegalArgumentException("inputPath:"+inputFolderName+" must be a directory!");
-    }
-
-    @Option(name = "-inputFile", aliases = {"-if" }, usage = "add an input hdr log from input path, also takes regexp", required = false)
-    public void addInputFile(String inputFile) {
-        final Predicate<String> predicate = Pattern.compile(inputFile).asPredicate();
-        inputFiles.addAll(Arrays.asList(inputPath.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return predicate.test(pathname.getName());
+        for (Histogram sum : sumByTag.values()) {
+            switch (summaryType) {
+            case PERCENTILES:
+                printPercentiles(out, sum, period);
+                break;
+            case CSV:
+                printCsv(out, sum);
+                break;
+            case HGRM:
+                printHgrm(out, sum);
+                break;
+            default:
+                throw new IllegalStateException();
             }
-        })));
-    }
-    @Option(name = "-inputFilePath", aliases = {"-ifp" }, usage = "add an input file by path relative to working dir or absolute", required = false)
-    public void addInputFileAbs(String inputFileName) {
-        File in = new File(inputFileName);
-        if (!in.exists())
-            throw new IllegalArgumentException("file:"+inputFileName+" must exist!");
-        inputFiles.add(in);
+        }
     }
 
-    @Option(name = "-outputFile", aliases = {"-of" }, usage = "set an output file destination, default goes to sysout", required = false)
-    public void setOutputFile(String outputFileName) {
-        outputFile = new File(outputFileName);
+    private void printHgrm(PrintStream out, Histogram sum) {
+        sum.outputPercentileDistribution(out, percentilesOutputTicksPerHalf, outputValueUnitRatio);
     }
 
+    private void printPercentiles(PrintStream out, Histogram sum, long period) {
+        double avgThpt = (sum.getTotalCount() * 1000.0) / period;
+        String tag = (sum.getTag() == null) ? "" : sum.getTag() + ".";
+        out.printf("%sTotalCount=%d\n", tag, sum.getTotalCount());
+        out.printf("%sPeriod(ms)=%d\n", tag, period);
+        out.printf("%sThroughput(ops/sec)=%.2f\n", tag, avgThpt);
+        out.printf("%sMin=%d\n", tag, (long) (sum.getMinValue() / outputValueUnitRatio));
+        out.printf("%sMean=%.2f\n", tag, sum.getMean() / outputValueUnitRatio);
+        out.printf("%s50.000ptile=%d\n", tag, (long) (sum.getValueAtPercentile(50) / outputValueUnitRatio));
+        out.printf("%s90.000ptile=%d\n", tag, (long) (sum.getValueAtPercentile(90) / outputValueUnitRatio));
+        out.printf("%s99.000ptile=%d\n", tag, (long) (sum.getValueAtPercentile(99) / outputValueUnitRatio));
+        out.printf("%s99.900ptile=%d\n", tag, (long) (sum.getValueAtPercentile(99.9) / outputValueUnitRatio));
+        out.printf("%s99.990ptile=%d\n", tag, (long) (sum.getValueAtPercentile(99.99) / outputValueUnitRatio));
+        out.printf("%s99.999ptile=%d\n", tag, (long) (sum.getValueAtPercentile(99.999) / outputValueUnitRatio));
+        out.printf("%sMax=%d\n", tag, (long) (sum.getMaxValue() / outputValueUnitRatio));
+    }
+
+    private void printCsv(PrintStream out, Histogram sum) {
+        long min = (long) (sum.getMinValue() / outputValueUnitRatio);
+        long max = (long) (sum.getMaxValue() / outputValueUnitRatio);
+        long bucketStart = (min / outputBucketSize) * outputBucketSize;
+        out.println("BucketStart, Count");
+        for (; bucketStart < max; bucketStart += outputBucketSize) {
+            long s = (long) (bucketStart * outputValueUnitRatio);
+            long e = (long) ((bucketStart + outputBucketSize) * outputValueUnitRatio);
+            long count = sum.getCountBetweenValues(s, e);
+            out.print(bucketStart);
+            out.print(",");
+            out.println(count);
+        }
+    }
 }
---
+++ src/main/java/UnionHistogramLogs.java
@@ -0,0 +1,260 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+import org.HdrHistogram.Histogram;
+import org.HdrHistogram.HistogramLogReader;
+import org.HdrHistogram.HistogramLogWriter;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+public class UnionHistogramLogs {
+
+    @Option(name = "-start", aliases = "-s", usage = "relative log start time in seconds, (default: 0.0)", required = false)
+    public double start = 0.0;
+
+    @Option(name = "-end", aliases = "-e", usage = "relative log end time in seconds, (default: MAX_DOUBLE)", required = false)
+    public double end = Double.MAX_VALUE;
+
+    @Option(name = "-verbose", aliases = "-v", usage = "verbose logging, (default: false)", required = false)
+    public boolean verbose = false;
+
+    @Option(name = "-relative", aliases = "-r", usage = "relative timeline merge, (default: true)", required = false)
+    public boolean relative = false;
+
+    @Option(name = "-inputPath", aliases = "-ip", usage = "set path to use for input files, defaults to current folder", required = false)
+    public void setInputPath(String inputFolderName) {
+        inputPath = new File(inputFolderName);
+        if (!inputPath.exists())
+            throw new IllegalArgumentException("inputPath:" + inputFolderName + " must exist!");
+        if (!inputPath.isDirectory())
+            throw new IllegalArgumentException("inputPath:" + inputFolderName + " must be a directory!");
+    }
+
+    @Option(name = "-inputFile", aliases = "-if", usage = "add an input hdr log from input path, also takes regexp", required = false)
+    public void addInputFile(String inputFile) {
+        final Predicate<String> predicate = Pattern.compile(inputFile).asPredicate();
+        inputFiles.addAll(Arrays.asList(inputPath.listFiles(pathname -> {
+            return predicate.test(pathname.getName());
+        })));
+    }
+
+    @Option(name = "-taggedInputFile", aliases = "-tif", usage = "a <tag>=<filename> add an input file, tag all histograms from this file with tag. If histograms have a tag it will be conactanated to file tag.", required = false)
+    public void addInputFileAbs(String inputFileNameAndTag) {
+        String[] args = inputFileNameAndTag.split("=");
+        if (args.length != 2)
+            throw new IllegalArgumentException("This value:" + inputFileNameAndTag + " should be a <tag>=<file>, niether tag nor filename allow the '=' char");
+        String tag = args[0];
+        String inputFileName = args[1];
+        File in = new File(inputFileName);
+        if (!in.exists())
+            throw new IllegalArgumentException("file:" + inputFileName + " must exist!");
+        inputFiles.add(in);
+        inputFilesTags.put(in, tag);
+    }
+
+    @Option(name = "-outputFile", aliases = "-of", usage = "set an output file destination, default goes to sysout", required = false)
+    public void setOutputFile(String outputFileName) {
+        outputFile = new File(outputFileName);
+    }
+
+    private File inputPath = new File(".");
+    private Set<File> inputFiles = new HashSet<>();
+    private Map<File, String> inputFilesTags = new HashMap<>();
+    private File outputFile;
+
+    public static void main(String[] args) throws Exception {
+        UnionHistogramLogs app = new UnionHistogramLogs();
+        CmdLineParser parser = new CmdLineParser(app);
+        try {
+            parser.parseArgument(args);
+            app.execute();
+        } catch (CmdLineException e) {
+            System.out.println(e.getMessage());
+            parser.printUsage(System.out);
+        }
+    }
+
+    private void execute() throws FileNotFoundException {
+        if (verbose) {
+            if (end != Double.MAX_VALUE)
+                System.out.printf("start:%.2f end:%.2f path:%s\n", start, end, inputPath.getAbsolutePath());
+            else
+                System.out.printf("start:%.2f end: MAX path:%s\n", start, inputPath.getAbsolutePath());
+
+            if (!inputFiles.isEmpty())
+                System.out.println("Reading files:");
+            else
+                System.out.println("No input files :(");
+
+            for (File inputFile : inputFiles) {
+                System.out.println(inputFile.getAbsolutePath());
+            }
+        }
+        if (inputFiles.isEmpty())
+            return;
+
+        PrintStream report = System.out;
+
+        if (outputFile != null) {
+            report = new PrintStream(new FileOutputStream(outputFile));
+        }
+        union(report);
+    }
+
+    class HistogramIterator implements Comparable<HistogramIterator> {
+        HistogramLogReader reader;
+        Histogram next;
+        String tag;
+        public HistogramIterator(HistogramLogReader reader) {
+            this.reader = reader;
+            read();
+        }
+        public HistogramIterator(HistogramLogReader reader, String tag) {
+            this.reader = reader;
+            this.tag = tag;
+            read();
+        }
+
+        private void read() {
+            next = (Histogram) reader.nextIntervalHistogram(start, end);
+            if (next == null)
+                return;
+
+            // replace start time with a relative one
+            if (relative) {
+                long length = next.getEndTimeStamp() - next.getStartTimeStamp();
+                long nextStartTime = (long) (next.getStartTimeStamp() - reader.getStartTimeSec() * 1000);
+                next.setStartTimeStamp(nextStartTime);
+                next.setEndTimeStamp(next.getStartTimeStamp() + length);
+            }
+            if (tag != null) {
+                String nextTag = next.getTag();
+                if (nextTag == null) {
+                    next.setTag(tag);
+                }
+                else {
+                    next.setTag(tag+"::"+nextTag);
+                }
+            }
+        }
+
+        Histogram next() {
+            Histogram c = next;
+            read();
+            return c;
+        }
+
+        boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public int compareTo(HistogramIterator o) {
+            if (!hasNext() && !o.hasNext())
+                return 0;
+            if (!hasNext())
+                return -1;
+            if (!o.hasNext())
+                return 1;
+            return (int) (next.getStartTimeStamp() - o.next.getStartTimeStamp());
+        }
+    }
+
+    private void union(PrintStream out) throws FileNotFoundException {
+        List<HistogramIterator> ins = new ArrayList<>();
+        for (File inputFile : inputFiles) {
+            ins.add(new HistogramIterator(new HistogramLogReader(inputFile), inputFilesTags.get(inputFile)));
+        }
+
+        ins.removeIf(e -> !e.hasNext());
+        Collections.sort(ins);
+
+        if (ins.isEmpty()) {
+            if (verbose) {
+                System.out.println("Input files do not contain range");
+            }
+            return;
+        }
+
+        // Init writer
+        HistogramLogWriter writer = new HistogramLogWriter(out);
+        writer.outputLogFormatVersion();
+        writer.outputComment("Union of:" + inputFiles + " start:" + start + " end:" + end + " relative:" + relative);
+        if (!relative) {
+            long startTimeStamp = (long) (ins.get(0).reader.getStartTimeSec() * 1000);
+            writer.setBaseTime(startTimeStamp);
+            writer.outputBaseTime(startTimeStamp);
+            writer.outputStartTime(startTimeStamp);
+        }
+        writer.outputLegend();
+
+        Map<String, Histogram> unionedByTag = new HashMap<>();
+        // iterators are now sorted by start time
+        while (!ins.isEmpty()) {
+            HistogramIterator input = ins.get(0);
+            Histogram next = input.next();
+            Histogram union = unionedByTag.computeIfAbsent(next.getTag(), k -> {
+                Histogram h = new Histogram(3);
+                h.setTag(k);
+                return h;
+            });
+
+            long nextStart = next.getStartTimeStamp();
+            long nextEnd = next.getEndTimeStamp();
+            long unionStart = union.getStartTimeStamp();
+            long unionEnd = union.getEndTimeStamp();
+            // iterators are sorted, so we know nextStart >= unionStart
+            boolean rollover = false;
+
+            if (unionStart == Long.MAX_VALUE) {
+                union.add(next);
+            } else if (nextStart < unionEnd && nextEnd <= unionEnd) {
+                union.add(next);
+            }
+            else if (nextStart < unionEnd) {
+                double nLength = nextEnd - nextStart;
+                double overlap = (unionEnd - nextStart) / nLength;
+                if(overlap > 0.8) {
+                    union.add(next);
+                    // prevent an ever expanding union
+                    union.setStartTimeStamp(unionStart);
+                    union.setEndTimeStamp(unionEnd);
+                }
+                else {
+                    rollover = true;
+                }
+            }
+            else {
+                rollover = true;
+            }
+            if (rollover) {
+                writer.outputIntervalHistogram(union);
+                union.reset();
+                union.setEndTimeStamp(0L);
+                union.setStartTimeStamp(Long.MAX_VALUE);
+                union.setTag(next.getTag());
+                union.add(next);
+            }
+            // trim and sort
+            ins.removeIf(e -> !e.hasNext());
+            Collections.sort(ins);
+        }
+        // write last hgrms
+        for (Histogram u : unionedByTag.values()) {
+            writer.outputIntervalHistogram(u);
+        }
+    }
+}
